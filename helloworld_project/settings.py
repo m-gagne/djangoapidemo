447737800f9af03b20b24dc7a560500fa0d5ec1a
m-gagne <marc.gagne@hotmail.com>
2018-04-25 08:21:13 -0400

    fixed database config to use propper mysql connector

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'helloworld_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'NAME': os.getenv('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
