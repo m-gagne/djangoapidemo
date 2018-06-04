fedbaa212cc25f854fc668c527e01f1060a68ee2
m-gagne <marc.gagne@hotmail.com>
2018-06-03 20:48:05 -0400

    updated

--- run_waitress_server.py
+++ run_waitress_server.py
@@ -1,7 +1,6 @@
-import os, sys
-from waitress import serve
-
-from helloworld_project.wsgi import application
-
-if __name__ == '__main__':
-    serve(application, host="0.0.0.0", port=os.getenv("PORT", 8000))
+import os
+from waitress import serve  
+from django.core.wsgi import get_wsgi_application  
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangosql.settings")  
+application = get_wsgi_application()  
+serve(application,host="0.0.0.0",port=os.getenv("PORT", 8000))
